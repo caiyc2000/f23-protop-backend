d27cb0abdd31a358c265e31476a70f26aa81460c
Jason Aposotl <102635962+jasona445@users.noreply.github.com>
2023-10-17 11:41:57 -0400

    sanity checks

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,11 +2,15 @@ from functions.functions import get_pdb, get_uniprot
 from functions.utils import get_uniprot_from_pdb
 import json
 
-pdb_dict = get_pdb("4HHB")
-print(json.dumps(pdb_dict, indent=4))
-
-uniprot_dict = get_uniprot_from_pdb(pdb_dict, "A")
-print(json.dumps(uniprot_dict, indent=4))
+#pdb_dict = get_pdb("4HHB")
+#print(json.dumps(pdb_dict, indent=4))
 
 uniprot_info = get_uniprot('Q92560')
-print(json.dumps(uniprot_info, indent=4))
+#print(json.dumps(uniprot_info['variants'], indent=4))
+
+for disease, mutants in uniprot_info['variants'].items():
+    for mutant in mutants:
+        if mutant['begin'] != mutant['end']:
+            print(disease, mutant)
+
+
